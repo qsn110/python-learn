507353155329d7fbbddbe1dde970fe638c9d0cba
qsn110 <1064673087@qq.com>
2018-06-12 14:23:28 +0800

    Update class.py
    add some content

--- class.py
+++ class.py
@@ -1,1 +1,15 @@
+class Student(object):
+    def __init__(self,name,score):
+        self.name = name
+        self.score = score
+    
+    def printScore(self):
+        print('%s: %s' % (self.name, self.score))
+        #print(self.score)
 
+student = Student("TOM",87)
+student.printScore()
+
+#x = input("please input x:")
+
+#print("the length of %s is %d" % (x,y))
